ce43ded9828a265948695fb040dc0dd5efd70334
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-07 12:18:13 +0530

    intro & running go programs & creating builds

---
+++ 01-intro/01-intro.go
@@ -0,0 +1,16 @@
+//package declaration
+package main
+
+//import dependency packages
+import "fmt"
+
+//package scoped variable/type declarations
+
+//package init function
+
+//main function
+func main() {
+	fmt.Println("Hello World!")
+}
+
+//other functions
