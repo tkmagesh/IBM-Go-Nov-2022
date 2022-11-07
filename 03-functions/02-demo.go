f863c3194dda1dda47179a05b940baff25675dc4
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-07 16:38:12 +0530

    variadic functions

---
+++ 03-functions/02-demo.go
@@ -0,0 +1,20 @@
+/* variadic functions */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(10))             //=> 10
+	fmt.Println(sum(10, 20))         //=> 30
+	fmt.Println(sum(10, 20, 30, 40)) //=> 100
+	fmt.Println(sum())               //=> 0
+}
+
+func sum(nos ...int) int {
+	result := 0
+	for i := 0; i < len(nos); i++ {
+		result += nos[i]
+	}
+	return result
+}
