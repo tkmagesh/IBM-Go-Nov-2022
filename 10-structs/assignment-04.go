c13d66c3e7ba637c0a6088900f7d110f220827f7
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-09 10:36:02 +0530

    assignment-04 - problem statement

---
+++ 10-structs/assignment-04.go
@@ -0,0 +1,6 @@
+/*
+	Create a struct Product with Id, Name, Cost, Category fields
+	Create the following functions
+		PrintProduct -> prints all the field values of the given product
+		ApplyDiscount -> Apply the given discount(%) on the given product (Cost)
+*/
