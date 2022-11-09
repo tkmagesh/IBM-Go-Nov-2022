51e7a2a9fd141e818a8d27e67075743b147264f9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-09 10:59:39 +0530

    assignment-04 - solution

--- 10-structs/assignment-04.go
+++ 10-structs/assignment-04.go
@@ -4,3 +4,29 @@
 		PrintProduct -> prints all the field values of the given product
 		ApplyDiscount -> Apply the given discount(%) on the given product (Cost)
 */
+
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Category string
+}
+
+func PrintProduct(product Product) {
+	fmt.Printf("Id = %d, Name = %q, Cost = %v, Category = %q\n", product.Id, product.Name, product.Cost, product.Category)
+}
+
+func ApplyDiscount(product *Product, discountPercent float32) {
+	product.Cost = product.Cost * ((100 - discountPercent) / 100)
+}
+
+func main() {
+	pen := Product{Id: 100, Name: "Pen", Cost: 10, Category: "Stationary"}
+	PrintProduct(pen)
+	ApplyDiscount(&pen, 10)
+	PrintProduct(pen)
+}
